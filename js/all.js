a51e0e4c2e3deda4a708accaa26951e94aa8f4dc
Zora <a121622855@gmail.com>
2016-06-13 18:25:53 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -4,7 +4,9 @@ $(function(){
 		showIndex = 0,			// 預設要先顯示那一張
 		fadeOutSpeed = 2000,	// 淡出的速度
 		fadeInSpeed = 3000,		// 淡入的速度
-		defaultZ = 10;			// 預設的 z-index
+		defaultZ = 10,			// 預設的 z-index
+		isHover = false,
+		timer, speed = 2000;	// 計時器及輪播切換的速度
  
 	// 先把其它圖片的變成透明
 	$ad.css({
@@ -29,7 +31,12 @@ $(function(){
 		showIndex = $(this).text() * 1 - 1;
  
 		// 顯示相對應的區域並把其它區域變成透明
-		$ad.eq(showIndex).stop().fadeTo(fadeInSpeed, 1).css('zIndex', defaultZ).siblings('a').stop().fadeTo(fadeOutSpeed, 0).css('zIndex', defaultZ - 1);
+		$ad.eq(showIndex).stop().fadeTo(fadeInSpeed, 1, function(){
+			if(!isHover){
+				// 啟動計時器
+				timer = setTimeout(autoClick, speed + fadeInSpeed);
+			}
+		}).css('zIndex', defaultZ).siblings('a').stop().fadeTo(fadeOutSpeed, 0).css('zIndex', defaultZ - 1);
 		// 讓 a 加上 .on
 		$(this).addClass('on').siblings().removeClass('on');
  
@@ -37,5 +44,24 @@ $(function(){
 	}).focus(function(){
 		$(this).blur();
 	}).eq(showIndex).addClass('on');
+ 
+	$ad.hover(function(){
+		isHover = true;
+		// 停止計時器
+		clearTimeout(timer);
+	}, function(){
+		isHover = false;
+		// 啟動計時器
+		timer = setTimeout(autoClick, speed);
+	})
+ 
+	// 自動點擊下一個
+	function autoClick(){
+		if(isHover) return;
+		showIndex = (showIndex + 1) % $controlA.length;
+		$controlA.eq(showIndex).click();
+	}
+ 
+	// 啟動計時器
+	timer = setTimeout(autoClick, speed);
 });
-
